0cd33cdc34ba169453b3ef561b77766a3933456e
jcarlos0462 <carlos_villegas@outlook.es>
2026-01-31 23:18:14 -0600

    nuevo

--- Registro_Daños.php
+++ Registro_Daños.php
@@ -207,5 +207,6 @@ $severidades = $conn->query("SELECT CodSeveridadDano FROM severidaddano ORDER BY
         </div>
     </div>
 </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
