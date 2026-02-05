f2c9c8b8daa3b54d198c106891ea87cd59f5a0f7
jcarlos0462 <carlos_villegas@outlook.es>
2026-02-05 16:42:57 -0600

    nuevo

--- reportes_vehiculos.php
+++ reportes_vehiculos.php
@@ -615,7 +615,7 @@ if ($has_filter) {
                         <div class="col-12 mt-2 no-print">
                             <button type="submit" class="btn btn-primary">Generar reporte</button>
                             <button type="submit" name="export_xml" value="1" formaction="reportes_vehiculos.php" formmethod="post" class="btn btn-warning ms-2">Exportar XLS (XML)</button>
-                            <a href="<?php echo htmlspecialchars($print_url); ?>" class="btn btn-secondary ms-2">Imprimir PDF</a>
+                            <button type="button" id="btnPrint" class="btn btn-secondary ms-2">Imprimir PDF</button>
                         </div>
                     </form>
                 </div>
@@ -703,6 +703,18 @@ if ($has_filter) {
     window.onload = function() { setTimeout(function(){ window.print(); }, 800); };
 </script>
 <?php endif; ?>
+<script>
+// Attach print handler to button so it doesn't trigger a page reload/search
+document.addEventListener('DOMContentLoaded', function(){
+    var btn = document.getElementById('btnPrint');
+    if (btn) {
+        btn.addEventListener('click', function(e){
+            // small delay to allow any UI changes before printing
+            setTimeout(function(){ window.print(); }, 250);
+        });
+    }
+});
+</script>
 </body>
 </html>
 <?php $conn->close(); ?>
