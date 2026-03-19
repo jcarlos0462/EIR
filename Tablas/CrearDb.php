9151e2ee3de5dda3303eb094d049f52c50b77ad3
jcarlos0462 <carlos_villegas@outlook.es>
2026-03-19 09:41:26 -0600

    nuevo

--- Tablas/CrearDb.php
+++ Tablas/CrearDb.php
@@ -1,20 +1,7 @@
 <?php
-// Conexión a la base de datos en Hostinger
-$servername = "localhost";
-$username = "u174025152_Administrador";
-$password = "0066jv_A2";
-$dbname = "u174025152_EIR";
+USE u174025152_EIR;
 
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-
-// Crear tabla vehiculo
-$sql = "CREATE TABLE IF NOT EXISTS vehiculo (
+CREATE TABLE IF NOT EXISTS vehiculo (
     ID INT(10) PRIMARY KEY AUTO_INCREMENT,
     Buque VARCHAR(25) NOT NULL,
     Viaje VARCHAR(25) NOT NULL,
@@ -25,13 +12,5 @@ $sql = "CREATE TABLE IF NOT EXISTS vehiculo (
     Año VARCHAR(10),
     Puerto VARCHAR(30),
     Terminal VARCHAR(20)
-)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Tabla 'vehiculo' creada exitosamente";
-} else {
-    echo "Error al crear tabla: " . $conn->error;
-}
-
-$conn->close();
+);
 ?>
